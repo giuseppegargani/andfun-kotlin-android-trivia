00ac27dce50ae4a6280626cedc6cdaea0e49a651
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-06-28 14:18:08 +0200

    PRIMA COMMIT

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -27,6 +27,54 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
+/*TODO - 1 Si verifica che esiste il menù (icona) per nel caso di vittoria
+    Si deve verificare anche la presenza della icona
+    e che abbia anche ShowAsAction = "ifRoom"
+ */
+
+/*TODO - 2 SI IMPOSTA LA PRESENZA DEL MENU'
+      setHasOptionsMenu(true) e deve andare dentro onCreate
+ */
+
+/* TODO - 3 Creare il menù
+    si deve fare override di onCreateOptionsMenu e si fà inflater del menù desiderato
+    // Showing the Share Menu Item Dynamically
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+       // hide the menu item if it doesn't resolve
+       menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+ */
+
+/*TODO - 4 Si crea la funzione getShareIntent() e si inizializza args con bundle da gameWon, intento SEND, stringa apposita
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+    return shareIntent
+    }
+ */
+
+/*TODO - 5 Si crea la funzione che materialmente lancia l'intento (shareSuccess()) (e ha come argomento una funzione di callback per intento)
+    startActivity(getShareIntent())
+
+ */
+
+/*TODO - 6 Si impostano le opzioni del menù
+    e che lancia la funzione shareSuccess()
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item!!.itemId) {
+       R.id.share -> shareSuccess()
+   }
+    return super.onOptionsItemSelected(item)
+    }
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
