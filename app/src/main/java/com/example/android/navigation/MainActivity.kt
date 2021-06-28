e9d091f44ec06a2390b199dbee766c8cdd8aae67
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-06-28 10:19:15 +0200

    SECONDA COMMIT

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -27,6 +27,38 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
+/* TODO - 1 Verifica che la dipendenza per Safe-Args sia stata correttamente scritta
+        prima si verifica che sia corretta la versione di safe-args nella build Gradle per il progetto
+        "android.arch.navigation:navigation-safe-args-gradle-plugin:$version_navigation"
+        e poi aggiungi il plugin nella build Gradle di App:
+        apply plugin: 'kotlin-kapt'
+        apply plugin: 'androidx.navigation.safeargs'
+ */
+
+/*TODO - 2 Sostituisci le righe di codice di navigazione tra frammenti con le corrispondenti istruzioni con SafeArgs
+    Sia in Game-fragment che in Game-Over
+    view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+ */
+
+/*TODO - 3 Nel frammento di destinazione inserisci i parametri desiderati per il controllo
+    in questo caso il frammento di destinazione è gameWon e perciò si vanno ad inserire lì
+    Si inseriscono da NavGraph
+    Si può verificare il nome dei parametri non è importante?!?!?!?
+ */
+
+/* TODO - 4 Inseriamo i parametri all'interno della istruzione di navigazione
+    view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
+ */
+
+/*TODO - 5 Utilizza i parametri passati in Safe-args per lanciare un Toast con un messaggio
+    val args = GameWonFragmentArgs.fromBundle(arguments!!)
+    Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+ */
+
+/*TODO - 6 Correggi NavDirections in qualsiasi azione di navigazione
+
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
