2b4e1582c092f55f83fc39149c4fa78b382ed999
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-06-29 16:37:33 +0200

    SECONDA COMMIT

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -21,6 +21,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.ActivityMainBinding
 
+/*TODO - 1 Controlla la dipendenza
+    Ultima dipendenza è
+    version_navigation = '1.0.0'
+    ____________________________________________________________________________________________
+    implementation "android.arch.navigation:navigation-fragment-ktx:$version_navigation"
+    implementation "android.arch.navigation:navigation-ui-ktx:$version_navigation"
+    ____________________________________________________________________________________________
+ */
+
+/* TODO - 2 Aggiungi il NavGraph e dagli il nome di navigation
+ */
+
+/*TODO - 3 inserisci il navhost al posto del frammento nella activity principale
+        metti come nome "myNavHost
+        come name metti "android:name="androidx.navigation.fragment.NavHostFragment"
+        come defaultNavHost metti true
+ */
+
+/*TODO - 4 Aggiungi il title e il GameFragment al navGraph
+ */
+
+/*TODO - 5 Aggiungi il bottone e fai il binding del bottone
+        binding.playButton.setOnClickListener(
+        Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        oppure la versione semplificata è:
+        Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+ */
+
+/*TODO - 6 SINTESI:
+    La cosa più importante è come collegare con un eventListener su un pulsante delle istruzioni di navigazione
+    ma non toglie che gli eventi possano essere anche altri
+ */
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
